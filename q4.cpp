2fbf796e24bc60e8314a030e99aaee644ed8f70b
Nadav <Nadavdanan3@gmail.com>
2022-07-27 00:14:43 +0300

    wrong file - fixedgit status!

--- q4.cpp
+++ q4.cpp
@@ -1,30 +1,52 @@
+#include <cstring>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <direct.h>
 #include <stdlib.h>
+
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#include <experimental/filesystem>
+
 #define _CRT_SECURE_NO_WARNINGS
 using namespace std;
+string working_directory = "Files";
+string path = "Files";
 
-void WriteToFile()
+#ifdef _linux_ 
+#include <unistd.h>
+    void MyMkDir(const char* path){
+		mkdir(path, 0666);
+	}
+#elif _WIN32
+#include <direct.h>
+    void MyMkDir(const char* path) {
+        _mkdir(path);
+    }
+#else
+
+#endif
+
+
+void WriteToFile(experimental::filesystem::path path)
 {
     ofstream myFile_Handler;
-
     // File Open
-    myFile_Handler.open("argument2.txt");
-    myFile_Handler << "Hello  from *argument3*" << endl;
+    myFile_Handler.open(path.c_str());
+    
+    myFile_Handler << "Hello from argument3" << endl;
 
     // File Close
     myFile_Handler.close();
 }
-void ReadFromFile()
+
+void ReadFromFile(experimental::filesystem::path path)
 {
     ifstream myFile_Handler;
     string myLine;
 
-    myFile_Handler.open("argument2.txt");
+    myFile_Handler.open(path.c_str());
     if (myFile_Handler.is_open())
     {
         // Keep reading the file
@@ -41,26 +63,39 @@ void ReadFromFile()
         cout << "Unable to open the file!";
     }
 }
-void create_dir()
-{
-    if (_mkdir("Files") == -1)
-        cout << "directory not opened" << endl;
 
-    else
-        cout << "Directory created";
+void create_dir(std::experimental::filesystem::path dir_path)
+{
+    if(false == std::experimental::filesystem::is_directory(dir_path)){
+    	if(false == std::experimental::filesystem::create_directory(dir_path))
+		cout << "directory creation failed" << endl;
+//		exit(1);
+    }
 }
+
 int main(int argc, char** argv)
 {
-    create_dir();
-    if (_chdir("Files") == -1)
-        cout << "directory not changed" << endl;
-
-    else
-        cout << "Directory entered";
-    WriteToFile();
-    ReadFromFile();
+    if (argc < 2){
+    	printf("missing argument\r\n");
+	    exit(1);
+    }
+    experimental::filesystem::path working_dir = experimental::filesystem::current_path();
+    working_dir /= working_directory;
     
-	std::cout << "Press enter to exit" << std::endl;
-	
+    create_dir(working_dir);
+    
+    experimental::filesystem::path path = working_dir;
+    path /= "argument2.txt";
+    if(!strcmp("create", argv[1]))
+    {
+	    WriteToFile(path);
+    }
+    if(!strcmp("read", argv[1]))
+    {
+    	ReadFromFile(path);
+    }
+    
+    std::cout << "Press enter to exit" << std::endl;
+    
     return 0;
 }
